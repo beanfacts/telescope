9c590472401dc581004825ba845edc3e4e48cee1
KagErou710 <kageyama710@gmail.com>
2021-04-16 17:50:07 +0700

    faster capturing mouse movement

--- src/client/telescope_client.c
+++ src/client/telescope_client.c
@@ -48,11 +48,6 @@ typedef struct {
 } screen_param;
 
 
-pthread_mutex_t lock;
-
-static void setCursor (xcb_connection_t*, xcb_screen_t*, xcb_window_t, int);
-static void testCookie(xcb_void_cookie_t, xcb_connection_t*, char *); 
-
 static void testCookie (xcb_void_cookie_t cookie, xcb_connection_t *connection,
     char *errMessage)
 {   
@@ -64,62 +59,20 @@ static void testCookie (xcb_void_cookie_t cookie, xcb_connection_t *connection,
     }
 }   
 
-
-static void setCursor (xcb_connection_t *connection,
-    xcb_screen_t     *screen,
-    xcb_window_t      window,
-    int               cursorId)
-{
-    xcb_font_t font = xcb_generate_id (connection);
-    xcb_void_cookie_t fontCookie = xcb_open_font_checked (connection,
-                                                            font,
-                                                            strlen ("cursor"),
-                                                            "cursor" );
-    testCookie (fontCookie, connection, "can't open font");
-
-    xcb_cursor_t cursor = xcb_generate_id (connection);
-    xcb_create_glyph_cursor (connection,
-                                cursor,
-                                font,
-                                font,
-                                cursorId,
-                                cursorId + 1,
-                                0, 0, 0, 0, 0, 0 );
-
-    xcb_gcontext_t gc = xcb_generate_id (connection);
-
-    uint32_t mask = XCB_GC_FOREGROUND | XCB_GC_BACKGROUND | XCB_GC_FONT;
-    uint32_t values_list[3];
-    values_list[0] = screen->black_pixel;
-    values_list[1] = screen->white_pixel;
-    values_list[2] = font;
-
-    xcb_void_cookie_t gcCookie = xcb_create_gc_checked (connection, gc, window, mask, values_list);
-    testCookie (gcCookie, connection, "can't create gc");
-
-    mask = XCB_CW_CURSOR;
-    uint32_t value_list = cursor;
-    xcb_change_window_attributes (connection, window, mask, &value_list);
-
-    xcb_free_cursor (connection, cursor);
-
-    fontCookie = xcb_close_font_checked (connection, font);
-    testCookie (fontCookie, connection, "can't close font");
-}
-
-
-void get_center(xcb_connection_t *c, xcb_window_t window, int *ret_cenx, int *ret_ceny) {
-
+void get_param(xcb_connection_t *c, xcb_window_t window, int *ret_cenx, int *ret_ceny, int *ret_left, int *ret_right, int *ret_upper, int *ret_bottom) {
     xcb_get_geometry_cookie_t cookie;
     xcb_get_geometry_reply_t *reply;
 
-    cookie = xcb_get_geometry_unchecked(c, window);
-
+    cookie = xcb_get_geometry(c, window);
+    
     if ((reply = xcb_get_geometry_reply(c, cookie, NULL))) {
-        *ret_cenx = (reply->width)/2;
-        *ret_ceny = (reply->height)/2;   
+		*ret_cenx = (reply->width)/2;
+		*ret_ceny = (reply->height)/2;
+        *ret_left = 100;
+        *ret_right = (reply->width)-100;
+        *ret_upper = 50;
+        *ret_bottom = (reply->height)-50;        
     }
-
     free(reply);
 }
 
@@ -146,132 +99,116 @@ void send_input(int sockfd, char *buff, int size)
 
 void *capture_mouse_keyboard(void *args) {
 
-    mouse_param *mouse_kb = (mouse_param*) args;
-            
-    int cenx, ceny;  
+    mouse_param *mouse_kb = (mouse_param*) args;      
+    int cenx, ceny; 
     xcb_xfixes_query_version(mouse_kb->c,4,0);
     xcb_xfixes_show_cursor(mouse_kb->c, mouse_kb->x11_window);
     int flag = 0;
     xcb_generic_event_t *event;
-    
-    
     int16_t pre_x, pre_y, new_x, new_y, deltax, deltay = 0;
-
+    int left, right, upper, bottom;
     char *mouse_mov = malloc(9);
     char *mouse_but = malloc(6);
     char *keyboard = malloc(6);
-    
+
     while(event = xcb_wait_for_event (mouse_kb->c)) {
 
         switch (event->response_type & ~0x80)
         {
             case XCB_MOTION_NOTIFY: {
-            
             xcb_motion_notify_event_t *motion = (xcb_motion_notify_event_t *)event;
-            get_center(mouse_kb->c, mouse_kb->x11_window, &cenx, &ceny);
-            
+            get_param(mouse_kb->c, mouse_kb->x11_window, &cenx, &ceny, &left, &right, &upper, &bottom);
             // Calculate the amount that the mouse move
 
             new_x = motion->event_x;	
             new_y = motion->event_y;
-
             deltax = new_x - pre_x;
             deltay = new_y - pre_y;
-
             pre_x = motion->event_x;
             pre_y = motion->event_y;
-            
-            
-            if (((deltax != 0) && (deltay != 0)) && (flag == 0)) {
-                
+
+
+
+            if ((left >= pre_x)||(pre_x>=right)||(upper>=pre_y)||(bottom<=pre_y)) {    
+                printf("warp\n");
+                int pre_x = cenx;
+                int pre_y = ceny;
+                int new_x = cenx;
+                int new_y = ceny;
+                int deltax = 0;
+                int deltay = 0;
+
                 flag = 1;
-                printf("(%d : %d)\n", deltax, deltay);
-
-                mouse_mov[0] = (char) 0;
-                *(int *) (&mouse_mov[1]) = deltax;
-                *(int *) (&mouse_mov[5]) = deltay;
-
-                send_input(mouse_kb->sockfd, mouse_mov, 9); 
-                memset(mouse_mov, 0, 9);
+                xcb_warp_pointer(mouse_kb->c, mouse_kb->x11_window, mouse_kb->x11_window, 0,0,0,0, cenx, ceny);
+                
             }
-            else if(flag == 1){
+            else{
+                if(flag == 0){
+            printf("(%d : %d)\n", deltax, deltay);
+
+            flag = 1;
+            printf("(%d : %d)\n", deltax, deltay);
+            mouse_mov[0] = (char) 0;
+            *(int *) (&mouse_mov[1]) = deltax;
+            *(int *) (&mouse_mov[5]) = deltay;
+            send_input(mouse_kb->sockfd, mouse_mov, 9); 
+            memset(mouse_mov, 0, 9);
+
+            }
+            else{
                 flag = 0;
             }
-
-            xcb_warp_pointer(mouse_kb->c, mouse_kb->x11_window, mouse_kb->x11_window, 0,0,0,0, cenx, ceny); 
+            }
             break;
-                
             }
 
             case XCB_BUTTON_RELEASE: {
                 xcb_button_release_event_t *br = (xcb_button_release_event_t *)event;
-                
-
                 printf ("Button %d released\n", br->detail);
-
                 mouse_but[0] = (char) 1;
-
                 int *ptr6 = (int *)(&mouse_but[1]);
                 *ptr6 = br->detail;
-
                 mouse_but[5] = (char) 0;
                 send_input(mouse_kb->sockfd, mouse_but, 6); 
                 memset(mouse_but, 0, 6);
-
                 break;
             }
 
             case XCB_KEY_PRESS: {
-                xcb_key_press_event_t *kp = (xcb_key_press_event_t *)event;
-                
+                xcb_key_press_event_t *kp = (xcb_key_press_event_t *)event;           
                 printf ("Key pressed in window %d\n", kp->detail);
-
                 keyboard[0] = (char) 2;
-
                 int *ptr = (int *)(&keyboard[1]);
                 *ptr =  kp->detail;
-
                 keyboard[5] = (char) 1;
-
                 send_input(mouse_kb->sockfd, keyboard, 6);
                 memset(keyboard, 0, 6);
-
                 break;
             }
 
             case XCB_KEY_RELEASE: {
                 xcb_key_release_event_t *kr = (xcb_key_release_event_t *)event;
-                
-
                 printf ("Key released in window %d\n", kr->detail);
-
                 keyboard[0] = (char) 2;
-
                 int *ptr = (int *)(&keyboard[1]);
                 *ptr =  kr->detail;
-
                 keyboard[5] = (char) 0;
                 send_input(mouse_kb->sockfd, keyboard, 6);
                 memset(keyboard, 0,6);
                 break;
             }
 
-            // todo: will fix later
-            
             case XCB_ENTER_NOTIFY:{
-                /*xcb_xfixes_hide_cursor(mouse_kb->c, mouse_kb->screen->root);
+                xcb_xfixes_hide_cursor(mouse_kb->c, mouse_kb->x11_window);
                 printf("enter\n");
-                break;*/
-                break;
+                break;              
             }
 
             case XCB_LEAVE_NOTIFY:{
-                /*xcb_xfixes_show_cursor(mouse_kb->c, mouse_kb->screen->root);
-                setCursor (mouse_kb->c, mouse_kb->screen, mouse_kb->xcb_window, 58);
-                
+                xcb_xfixes_show_cursor(mouse_kb->c, mouse_kb->x11_window);
                 printf("leave\n");
-                break;*/
                 break;
+                
             }
             case XCB_BUTTON_PRESS: {
                 xcb_button_press_event_t *bp = (xcb_button_press_event_t *)event;
@@ -298,10 +235,9 @@ void *capture_mouse_keyboard(void *args) {
        
 }
 
-
 XImage *create_ximage(Display *display, Visual *visual, int width, int height, char *image)
 {
-    return XCreateImage(display, visual, 24, ZPixmap, 0, image, width, height, 32, 0);
+    return XCreateImage(display, visual, 24,ZPixmap, 0, image, width, height, 32, 0);
 }
 
 int putimage(char *image, Display *display, Visual *visual, Window window, GC gc)
@@ -311,15 +247,13 @@ int putimage(char *image, Display *display, Visual *visual, Window window, GC gc
     bool exit = false;
     int r;
     r = XPutImage(display, window, gc, ximage, 0, 0, 0, 0, WIDTH, HEIGHT);
-    if (r) {
-        print("Error occurred while displaying X11 image (%d)", r);
-    }
+    printf("RES: %i\n", r);
     return 0;
 }
 
-
 void *func(void *args) 
 {
+    //screen_param *cap_screen
     screen_param *cap_screen = (screen_param *) args;
     char *buff=calloc(MAX,1); 
     int n; 
@@ -340,9 +274,7 @@ void *func(void *args)
         }
         else
         {
-            fprintf(stderr, "Error reading from socket.\n");
-            exit(1);
-            // should make it exit gracefully but problem for future
+            //fprintf(stderr, "Error reading from socket.\n");
         }
     } 
 } 
@@ -356,8 +288,7 @@ int main(int argc, char *argv[])
         exit(1);
     }
     else
-    {
-        
+    {       
         Status mt_status;
         mt_status = XInitThreads();
         if (!mt_status) {
@@ -405,15 +336,13 @@ int main(int argc, char *argv[])
                                     | XCB_EVENT_MASK_LEAVE_WINDOW);
         XStoreName(display, window, "X11 window");
         XMapWindow(display, window);
-        XFlush(display);
-        
+        XFlush(display);  
         gc = XCreateGC(display, window, 0, NULL);
         
         /* Create connection objects for RDMA + TCP */
 
         int sockfd, connfd; 
         struct sockaddr_in servaddr, cli;
-
         int sockfd2, connfd2; 
         struct sockaddr_in servaddr2, cli2; 
         int flags = 1;
@@ -474,8 +403,6 @@ int main(int argc, char *argv[])
         screen_param* screen_config = calloc(1, sizeof(screen_param));
 
         mouse_config->c = c;
-        //mouse_config->screen = screen2;
-        //mouse_config->xcb_window = xcb_window;
         mouse_config->x11_window = window;
         mouse_config->sockfd = sockfd2;
         mouse_config->connfd = connfd2;
@@ -497,4 +424,4 @@ int main(int argc, char *argv[])
 
         exit(0);
     }
-}
+} 
