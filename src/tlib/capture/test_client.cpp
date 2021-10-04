b0e1359ccbba9028cb7e07fb5c27fb2a07716715
maomaocake <tangpansak@gmail.com>
2021-10-04 09:30:26 +0700

    test client commit

---
+++ src/tlib/capture/test_client.cpp
@@ -0,0 +1,126 @@
+//
+// Created by maomao on 03/10/2021.
+//
+
+// cout and stuff
+#include <iostream>
+
+//x11
+#include <sys/shm.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/extensions/XShm.h>
+
+//opengl stuff
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#include "capture_base.h"
+#include "capture_x11.h"
+
+
+//Display         *dpy;
+GLFWwindow      *win;
+int             dsp_width = 1920;
+int             dsp_height = 1080;
+XImage			*xim;
+GLuint			texture_id;
+// these are used for scaling
+int             width;
+int             height;
+const float aspect_ratio = float(dsp_width)/dsp_height;
+
+void reshapeScene(GLint width, GLint height)
+{
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    int w = height * aspect_ratio;           // w is width adjusted for aspect ratio
+    int left = (width - w) / 2;
+    // moves the viewport around to get the screen size scaled
+    glViewport(left, 0, w, height);       // fix up the viewport to maintain aspect ratio
+    glMatrixMode(GL_MODELVIEW);
+
+    // glutPostRedisplay();
+}
+
+void Redraw() {
+    // evrytime we redraw the scene we get the window size and reshape accordingly
+    glfwGetWindowSize(win, &width, &height);
+    reshapeScene(width,height);
+    // clear the screen so we can draw another frame
+    glClearColor(0.3, 0.3, 0.3, 1.0);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // draw a rectangle
+    glBegin(GL_QUADS);
+    glTexCoord2f(0.0, 0.0); glVertex2f(-1.0,1.0);
+    glTexCoord2f(1.0, 0.0); glVertex2f( 1.0,1.0);
+    glTexCoord2f(1.0, 1.0); glVertex2f( 1.0,-1.0);
+    glTexCoord2f(0.0, 1.0); glVertex2f(-1.0,-1.0);
+    glEnd();
+
+    // draw onto screen
+    glfwSwapBuffers(win);
+}
+
+
+/*                */
+/*  MAIN PROGRAM  */
+/*                */
+int main(int argc, char *argv[]) {
+    int dsp = atoi(argv[1]);
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    win = glfwCreateWindow(dsp_width, dsp_height, "Telescope", nullptr, nullptr);
+    if (!win)
+    {
+        std::cout << "Error: Failed to Create glfw Window \n";
+        glfwTerminate();
+        return -1;
+    }
+
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(win);
+
+
+    //init glew
+    // glew gets the fuctions working for "new" opengl
+    if (glewInit()  != GLEW_OK){
+        std::cout << "Error: Failed to initialise Glew";
+    }
+
+    // enable features
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_TEXTURE_2D);
+
+    // create a texture and set the parameters
+    // idk if the parameters are needed but its there
+    glGenTextures(1, &texture_id);
+    glBindTexture(GL_TEXTURE_2D, texture_id);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+
+    // define and init the cap class
+    tsc_capture_x11 cap;
+    cap.init();
+    std::vector<tsc_display> screens = cap.get_displays();
+    // std::cout << screens << "\n";
+    tsc_frame_buffer *buf = cap.alloc_frame(dsp);
+
+    while (!glfwWindowShouldClose(win)){
+        // render and call the xshmget
+        cap.update_frame(buf);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, dsp_width, dsp_height, 0, GL_BGRA, GL_UNSIGNED_BYTE, buf->buffer);
+        Redraw();
+        glfwPollEvents();
+        
+    } /* while(1) */
+
+} /* int main(...) */
